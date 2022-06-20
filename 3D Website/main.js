cbd33d14d977256f2329c7afe79e0016a1853f40
Oroppo2 <dante.arruda@ontariotechu.net>
2022-06-20 14:31:38 -0400

    Shader Support

--- 3D Website/main.js
+++ 3D Website/main.js
@@ -3,17 +3,21 @@ import './style.css'
 import * as THREE from 'three';
 
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import * as dat from 'dat.gui';
 
 const scene = new THREE.Scene();
 
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+const gui = new dat.GUI();
+
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 2000 );
 
 const renderer = new THREE.WebGLRenderer({
-  canvas: document.querySelector('#bg'),
+ canvas: document.querySelector('canvas'),
 })
 
 renderer.setPixelRatio(window.devicePixelRatio );
 renderer.setSize(window.innerWidth, window.innerHeight);
+//document.body.appendChild(renderer,domElement)
 camera.position.setZ(30);
 
 renderer.render( scene, camera );
@@ -31,9 +35,15 @@ pointLight.position.set(5,5,5);
 const ambientLight = new THREE.AmbientLight(0xffffff);
 scene.add(pointLight, ambientLight);
 
-const lightHelper = new THREE.PointLightHelper(pointLight)
-const gridHelper = new THREE.GridHelper(200,50);
-scene.add (lightHelper, gridHelper)
+//const lightHelper = new THREE.PointLightHelper(pointLight)
+//const gridHelper = new THREE.GridHelper(200,50);
+//scene.add (lightHelper, gridHelper)
+
+const cameraFolder = gui.addFolder('Camera')
+
+cameraFolder.add(camera.position,'x')
+cameraFolder.add(camera.position,'y',-30,30,0.01)
+cameraFolder.add(camera.position,'z',-30,30,0.01)
 
 function addStar(){
   const geometry = new THREE.SphereGeometry(0.25,24,24);
@@ -51,7 +61,7 @@ Array(200).fill().forEach(addStar);
 const spaceTexture = new THREE.TextureLoader().load('space.jpg');
 scene.background = spaceTexture;
 
-const planetTexture = new THREE.TextureLoader().load('planet.jpg');
+const planetTexture = new THREE.TextureLoader().load('Earth.jpg');
 const normalTexture = new THREE.TextureLoader().load('NormalMap.jpg');
 
 const planet = new THREE.Mesh(
@@ -72,16 +82,17 @@ planet.attach(moon);
 
 
 
-
+/*
 function moveCamera(){
   const t = document.body.getBoundingClientRect().top;
-
+  
   camera.rotation.x = t * -0.0001;
   camera.rotation.y = t * -0.0002;
   camera.rotation.z = t * -0.0002;
 }
 
 document.body.onscroll = moveCamera;
+*/
 
 function animate(){
 
